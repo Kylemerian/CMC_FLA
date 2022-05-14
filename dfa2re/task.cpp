c7c67d9a5b1e4909a8a6e0f9c75b56e1d8fb62d4
Alexey <s02190176@gse.cs.msu.ru>
2022-05-14 17:16:12 +0300

    dfa2re

--- dfa2re/task.cpp
+++ dfa2re/task.cpp
@@ -3,7 +3,7 @@
 #include <set>
 #include <algorithm>
 #include <iostream>
-
+/*
 std::set<std::string> setQ(std::string s, std::set<std::pair<std::pair<std::string, std::string>, std::string>> d) {
     std::set<std::string> res;
     for(auto i : d)
@@ -12,7 +12,7 @@ std::set<std::string> setQ(std::string s, std::set<std::pair<std::pair<std::stri
     return res;
 }
 
-std::set<std::string> setP(std::string s, std::set<std::pair<std::pair<std::string, std::string>, std::string>> &d) {
+std::set<std::string> setP(std::string s, std::set<std::pair<std::pair<std::string, std::string>, std::string>> d) {
     std::set<std::string> res;
     for(auto i : d)
         if(i.first.first == s && i.first.second != s)
@@ -43,17 +43,19 @@ int isQP(std::string Q, std::string P, std::set<std::pair<std::pair<std::string,
     
 }
 
-void deleteQP(std::string Q, std::string P, std::set<std::pair<std::pair<std::string, std::string>, std::string>> d){
+void deleteQP(std::string Q, std::string P, std::set<std::pair<std::pair<std::string, std::string>, std::string>>& d){
     for(auto i : d){
         if(i.first.first == Q && i.first.second == P){
-            d.erase(i);
+            int a = d.erase(i);
             return;
         }
     }
 }
-
+*/
 std::string dfa2re(DFA &d) {
-    
+    std::cout << "was hwereewrrere\n";
+
+    /*
     std::set<std::pair<std::pair<std::string, std::string>, std::string>> trans;
 
     for(auto s1 : d.get_states()){
@@ -72,13 +74,17 @@ std::string dfa2re(DFA &d) {
     }
     std::set<std::string> states = d.get_states();
     std::set<std::string> notfin;
+    std::set<std::string> fin;
     for(auto i : states){
         if(d.is_final(i))
-            continue;
-        notfin.insert(i);
+            fin.insert(i);
+        else
+            notfin.insert(i);
     }
-
-    while(!notfin.empty()){
+    std::cout << "was hrere\n";
+-*/
+/*
+    while(notfin.size() != 1){
         std::set<std::string> q = setQ(*notfin.begin(), trans);
         std::set<std::string> p = setP(*notfin.begin(), trans);
 
@@ -87,23 +93,94 @@ std::string dfa2re(DFA &d) {
             for(auto j : p){
                 int qp = isQP(i, j, trans);
                 if(s && qp){
-
+                    std::string fir = findQP(i ,j, trans);
+                    std::string sec = findQP(i, *notfin.begin(), trans) + "(" + findQP(*notfin.begin(), *notfin.begin(), trans) + ")*" + findQP(*notfin.begin(), j, trans);
+                    deleteQP(i, j, trans);
+                    trans.insert(std::make_pair(std::make_pair(i, j), fir + "|" + sec));
+                    deleteQP(i, *notfin.begin(), trans);
+                    deleteQP(*notfin.begin(), j, trans);
                 }
                 if(s && !qp){
-
+                    trans.insert(std::make_pair(std::make_pair(i, j), findQP(i, *notfin.begin(), trans) + "(" + findQP(*notfin.begin(), *notfin.begin(), trans ) + ")*" + findQP(*notfin.begin(), j, trans)));
+                    deleteQP(i, *notfin.begin(), trans);
+                    deleteQP(*notfin.begin(), j, trans);
                 }
                 if(!s && qp){
-
+                    std::string fir = findQP(i ,j, trans);
+                    std::string sec = findQP(i, *notfin.begin(), trans) + findQP(*notfin.begin(), j, trans);
+                    deleteQP(i, j, trans);
+                    trans.insert(std::make_pair(std::make_pair(i, j), fir + "|" + sec));
+                    deleteQP(i, *notfin.begin(), trans);
+                    deleteQP(*notfin.begin(), j, trans);
                 }
                 if(!s && !qp){
+                    trans.insert(std::make_pair(std::make_pair(i, j), findQP(i, *notfin.begin(), trans) + findQP(*notfin.begin(), j, trans)));
+                    deleteQP(i, *notfin.begin(), trans);
+                    deleteQP(*notfin.begin(), j, trans);
+                }
+            }
+        }
+        std::cout << "was hrere\n";
 
+        notfin.erase(*notfin.begin());
+    } 
+    std::cout << "was hrere\n";
+
+    fin.insert(*notfin.begin());*/
+    /*
+    while(fin.size() != 1){
+        std::set<std::string> q = setQ(*fin.begin(), trans);
+        std::set<std::string> p = setP(*fin.begin(), trans);
+
+        int s = isS(*fin.begin(), trans);
+        for(auto i : q){
+            for(auto j : p){
+                int qp = isQP(i, j, trans);
+                if(s && qp){
+                    std::string fir = findQP(i ,j, trans);
+                    std::string sec = findQP(i, *fin.begin(), trans) + "(" + findQP(*fin.begin(), *fin.begin(), trans) + ")*" + findQP(*fin.begin(), j, trans);
+                    deleteQP(i, j, trans);
+                    trans.insert(std::make_pair(std::make_pair(i, j), fir + "|" + sec));
+                    deleteQP(i, *fin.begin(), trans);
+                    deleteQP(*fin.begin(), j, trans);
+                }
+                if(s && !qp){
+                    trans.insert(std::make_pair(std::make_pair(i, j), findQP(i, *fin.begin(), trans) + "(" + findQP(*fin.begin(), *fin.begin(), trans ) + ")*" + findQP(*fin.begin(), j, trans)));
+                    deleteQP(i, *fin.begin(), trans);
+                    deleteQP(*fin.begin(), j, trans);
+                }
+                if(!s && qp){
+                    std::string fir = findQP(i ,j, trans);
+                    std::string sec = findQP(i, *fin.begin(), trans) + findQP(*fin.begin(), j, trans);
+                    deleteQP(i, j, trans);
+                    trans.insert(std::make_pair(std::make_pair(i, j), fir + "|" + sec));
+                    deleteQP(i, *fin.begin(), trans);
+                    deleteQP(*fin.begin(), j, trans);
+                }
+                if(!s && !qp){
+                    trans.insert(std::make_pair(std::make_pair(i, j), findQP(i, *fin.begin(), trans) + findQP(*fin.begin(), j, trans)));
+                    deleteQP(i, *fin.begin(), trans);
+                    deleteQP(*fin.begin(), j, trans);
                 }
             }
         }
 
-        notfin.erase(*notfin.begin());
-    } 
-    
+        fin.erase(*fin.begin());
+    }
+*//*
+    std::cout << "was hrere\n";
+    for(auto i : trans){
+        //if(i.first.first == *fin.begin() || i.first.second == *fin.begin() || i.first.first == *notfin.begin() || i.first.second == *notfin.begin())
+            std::cout << i.first.first << " " << i.first.second << " " << i.second << "\n";
+          //  return i.second;
+    }
 
-    return d.to_string();
+    for(auto i : fin){
+        std::cout << "fin" << i << " ";
+    }
+    for(auto i : notfin){
+        std::cout << "notfin" << i << " ";
+    }
+*/
+    return "a";
 }
